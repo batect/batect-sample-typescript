775d59a8bd84c99c83c0ecdf12aaaf1a0b03f8be
Charles Korn <me@charleskorn.com>
2020-06-06 14:34:27 +1000

    Add dependency on joke service.

--- index.ts
+++ index.ts
@@ -1,10 +1,21 @@
+import fetch from "node-fetch";
 import * as express from "express";
 
 const app = express();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello from the API!");
+app.get("/", async (req, res) => {
+  const response = await fetch("http://joke-service");
+
+  if (!response.ok) {
+    res.sendStatus(503);
+    res.send(`Joke service call failed with HTTP ${response.status} (${response.statusText})`);
+    return;
+  }
+
+  const responseBody = await response.json();
+
+  res.send(`Hello from the API! The joke of the day is: ${responseBody.Joke.Opener} ${responseBody.Joke.Punchline}`);
 });
 
 app.listen(port, () => {
